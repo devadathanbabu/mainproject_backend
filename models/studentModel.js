5d049863cda44b5ec8496bf3b9bd419cdc791bda
devadathanbabu <devadathanbabu27@gmail.com>
2024-03-06 18:37:54 +0530

    model update

--- models/studentModel.js
+++ models/studentModel.js
@@ -1,17 +1,47 @@
 const mongoose=require("mongoose")
 const studentSchema=new mongoose.Schema(
     {
-        name:String,
-        admissionNo:String,
-        branch:String,
-        dob:String,
-        gender:String,
-        address:String,
-        phoneNo:String,
-        alternatePhoneNo:String,
-        email:String,
-        password:String,
-        
+       
+    name: { 
+        type: String, 
+        required: true 
+    },
+    admissionNo: { 
+        type: String, 
+        required: true 
+    },
+    branch: { 
+        type: String, 
+        required: true 
+    },
+    dob: { 
+        type: String, 
+        required: true 
+    },
+    gender: { 
+        type: String, 
+        required: true 
+    },
+    address: { 
+        type: String, 
+        required: true 
+    },
+    phoneNo: { 
+        type: String, 
+        required: true 
+    },
+    alternatePhoneNo: { 
+        type: String },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/ 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    }
 
     }
 )
